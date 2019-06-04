7a254ff150bd83233f96c15cf014d1afbdc951b7
lamecode <44145440+lamecode@users.noreply.github.com>
2019-06-04 20:29:49 +0300

    writting WAV file, but just white noise playing back

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,75 @@
 import audio_editor.Wave;
+import audio_editor.WaveReader;
+import audio_editor.WaveWritter;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-//        change_duration_in_integer_times();
-//        write_to_wav();
-        byte[] audioBytes = convert_to_byte_array();
-        Wave s = new Wave();
+    private static final String INPUT_FILE = "Game of Thrones.wav";
+    private static final String OUTPUT_FILE = "NewOne.wav";
 
+    public static void main(String[] args) {
+        byte[] fileBytesArr = readBytesFromFile(Paths.get(INPUT_FILE));
+        Wave wave = WaveReader.readFromByteArray(fileBytesArr);
+        multiplyWave(wave, getIntFromUser());
+        byte[] output = WaveWritter.writeToByteArray(wave);
+        writeBytesToFile(output);
     }
 
-    public static byte[] convert_to_byte_array() {
+    private static int getIntFromUser() {
+        int user = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введіть кількість раз, в яку хочете розтягнути аудіозапис:");
+        if (in.hasNextInt()) {
+            user = in.nextInt();
+        } else {
+            throw new RuntimeException("Invalid input");
+        }
+        return user;
+    }
+
+    private static byte[] readBytesFromFile(Path file) {
         try {
-            BufferedInputStream sound = new BufferedInputStream(new FileInputStream("Game of Thrones.wav"));
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            int read;
-            byte[] buff = new byte[1024];
-            while ((read = sound.read(buff)) > 0) {
-                out.write(buff, 0, read);
-            }
-            out.flush();
-            byte[] audioBytes = out.toByteArray();
-            return audioBytes;
+            return Files.readAllBytes(file);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
-    /*public static void write_to_wav(){
-
-    }*/
         return null;
     }
 
-    /*public static void change_duration_in_integer_times(byte[] a) {
-        byte[]
-        Wave newone = new Wave();
-    }*/
-
-    public static String GetChunkID(byte[] b) {
-        byte[] chunkSize = new byte[4];
-        for (int i = 0; i < 4; i++) {
-            chunkSize[i] = b[i];
-        }
-        String str = new String(chunkSize, 0, 4, StandardCharsets.UTF_8);
-        return str;
+    private static void multiplyWave(Wave wave, int times) {
+        byte[] multipliedData = getMultipliedData(wave, times);
+        wave.setData(multipliedData);
+        wave.setSubchunk2Size(wave.getSubchunk2Size() * times);
     }
+
+    private static byte[] getMultipliedData(Wave wave, int times) {
+        byte[] data = wave.getData();
+        byte[] multipliedData = new byte[data.length * times];
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < times; j++) {
+                if (data[i] < 0) {
+                    multipliedData[i * times + j] = data[i];
+                }
+                multipliedData[i * times + j] = data[i];
+            }
+
+        }
+        return multipliedData;
+    }
+
+    private static void writeBytesToFile(byte[] output) {
+        try {
+            Files.write(Paths.get(OUTPUT_FILE), output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Done");
+    }
+
 }
